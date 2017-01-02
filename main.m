f4836797b4a2ebf3a6f0026d3a67f6f563fa764a
jayleicn <jayleicn@gmail.com>
2017-01-02 06:19:22 -0600

    modify main

--- main.m
+++ main.m
@@ -1,8 +1,16 @@
 function map = main()
-ratio_array = linspace(1,10,10);
-map = zeros(10,1);
-for i=1:10
-   [ ~, ~, map_tmp] = DPSH(32, 'cifar10', ratio_array(i)*0.1);
-   map(i) = map_tmp;
+ratio_array = [0.1, 0.2];
+t = [1, 2, 5, 10, 20, 50];
+map = zeros(3,6);
+for i=1:2
+   for j=1:6
+   [ ~, ~, map_tmp] = transfer_hash(32, 'svhn', 'mnist', t(j), ratio_array(i));
+   map(i+1,j) = map_tmp;
+   map(1,j)=t(j);
+   end
 end
+fileID = fopen(['results/', 'mnist2svhn01_02.log'], 'w');
+fprintf(fileID, '%6s\t%6s\t%6s', 'tmep', 'map_01', 'map_02');
+fprintf(fileID, '%6d\t%4.2f\t%4.2f', map);
+fclose(fileID);
 end
