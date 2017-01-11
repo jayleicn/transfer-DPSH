6ea3f3e3c6908069b7ca7214a05fe86d97c2eab2
jayleicn <jayleicn@gmail.com>
2017-01-11 08:30:42 -0600

    add L2-norm

--- main.m
+++ main.m
@@ -1,13 +1,13 @@
 function map = main()
 ratio_array = [0.05, 0.1, 0.2];
 batchsize = [32, 32, 32];
-t = [0.5, 1, 2, 5, 10, 20];
+t = [1];%[ 0.5, 1, 2, 5, 10, 20];
 %eta = [0.001, 0.01, 0.1, 0.2, 0.5, 0.7, 1];
 l = length(t);
 map = zeros(4,l);
 for i=1:3
    for j=1:l
-     [ ~, ~, map_tmp] = transfer_hash(32, 'svhn', 'mnist', t(j), 0.1, ratio_array(i), batchsize(i));
+     [ ~, ~, map_tmp] = transfer_hash(32, 'svhn', 'mnist', t(j), 1, ratio_array(i), batchsize(i));
      map(i+1,j) = map_tmp;
      map(1,j)=t(j);
    end
